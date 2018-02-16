cd1cad6128d3219bd670c400b95937289f9cf1e8
balazs4 <balazs4web@gmail.com>
2018-02-16 11:11:08 +0100

    parcel react jsx

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import { render } from 'react-dom';
+import React from 'react';
+
+const App = () => <div>Hello from React, with parcel 1.6</div>;
+
+render(<App />, document.getElementById('app'));
