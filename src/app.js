5f59b258f39c58bc0bfabed734ab68fdf4ec22aa
balazs4 <balazs4web@gmail.com>
2018-02-16 11:55:56 +0100

    parcel preact js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-import { render } from 'react-dom';
-import React from 'react';
+import {h, render } from 'preact';
 
-const App = () => <div>Hello from React, with parcel 1.6</div>;
+const App = () => <div>Hello from preact, with parcel 1.6</div>;
 
 render(<App />, document.getElementById('app'));
